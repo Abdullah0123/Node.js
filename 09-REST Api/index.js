7250c674d456b9feff382f537376bd670309387d
Muhammad Abdullah <m.abdullah4475@gmil.com>
2024-03-04 21:19:34 +0500

    Implement REST api

--- 09-REST Api/index.js
+++ 09-REST Api/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const fs = require('fs')
 const users = require('./users')
 const app = express()
 const PORT = 8000
+
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/api/users', (req, res) => {
     return res.json(users)
@@ -14,16 +17,45 @@ app.get('/users', (req, res) => {
     return res.send(html)
 })
 
+app.post('/api/users', (req, res) => {
+    const user = req.body
+    user.id = users.length + 1
+    users.push(user)
+
+    fs.writeFile('users.json', JSON.stringify(users), (data, error) => {
+        return res.json({ status: 'Success', id: users.length })
+    })
+})
+
 app.route('/api/users/:id').get((req, res) => {
     const id = Number(req.params.id)
     const user = users.find(user => user.id === id)
     return res.json(user)
-}).post((req, res) => {
-    // ToDO: Add new user
-    return res.json({status: 'Pending'})
 }).patch((req, res) => {
+    // ToDO: Add new user
+    const user = req.body
+    const userId = req.params.id
+
+    if (Number(userId) <= users.length) {
+        users[userId - 1] = { id: Number(userId), ...user }
+        fs.writeFile('users.json', JSON.stringify(users), (data, error) => {
+            return res.json({ status: 'Success', data: users[userId - 1] })
+        })
+    } else {
+        return res.json({ status: 'Not fonund' })
+    }
+}).delete((req, res) => {
     // ToDO: Update user
-    return res.json({status: 'Pending'})
+    const userId = req.params.id
+
+    if (Number(userId) <= users.length) {
+        users.splice(userId - 1, 1)
+        fs.writeFile('users.json', JSON.stringify(users), (data, error) => {
+            return res.json({ status: 'Success'})
+        })
+    } else {
+        return res.json({ status: 'Not fonund' })
+    }
 })
 
 app.listen(PORT, () => console.log('Server started'))
